2a4df4b7a71ff2c9ebc35a5c75d3de1606c19273
Nisaruj Rattanaaram <nisaruj@hotmail.com>
2020-05-03 18:39:18 +0700

    Add spinning effect to coins

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Coin : Obtainable
 {
+    [Range(0f, 360f)][SerializeField] float m_RotationSpeed = 120f;
+
     public override void onObtain(GameObject player) 
     {
         Debug.Log("Obtained");
+    }
+
+    public void Update() 
+    {
+        transform.Rotate(0, m_RotationSpeed * Time.deltaTime, 0);
     }
 }
