eb1f2304387bef9753da52af3a9534719f2742a2
Nisaruj Rattanaaram <nisaruj@hotmail.com>
2020-05-03 12:32:02 +0700

    Add Obtainable object annd coin

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Obtainable
+{
+    public override void onObtain(GameObject player) 
+    {
+        Debug.Log("Obtained");
+    }
+}
